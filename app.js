b78369cda4713b8e69ad2f7e7d006e54ee7daa5e
Rahique <rahiqueosman@gmail.com>
2022-02-08 08:25:58 +0600

    2nd commit

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-let leads = [];
+let myLeads = [];
 const inputEL = document.querySelector(".input");
 const printEl = document.querySelector(".content");
 const saveButtonEl = document.querySelector(".save__button");
 const deleteButtonEl = document.querySelector(".delete__button");
 // -----------------------------------------------------
-const getData = JSON.parse(localStorage.getItem("myLeads"));
-if (getData) {
-  leads = getData;
-  renderLead();
+const gettingData = JSON.parse(localStorage.getItem("dataStore"));
+
+if (gettingData) {
+  myLeads = gettingData;
+  render(myLeads);
 }
 // Function & Listener
-saveButtonEl.addEventListener("click", () => {
-  leads.push(inputEL.value);
-  inputEL.value = null;
-  saveData();
-  renderLead();
-});
-deleteButtonEl.addEventListener("dblclick", () => {
-  printEl.innerHTML = null;
-  localStorage.clear();
-  leads = [];
-});
-
-function renderLead() {
+function render(leads) {
   let listItem = "";
   for (let i = 0; i < leads.length; i++) {
     listItem += `
@@ -34,7 +23,18 @@ function renderLead() {
   }
   printEl.innerHTML = listItem;
 }
+deleteButtonEl.addEventListener("dblclick", () => {
+  printEl.innerHTML = null;
+  localStorage.clear();
+  myLeads = [];
+});
+saveButtonEl.addEventListener("click", () => {
+  myLeads.push(inputEL.value);
+  inputEL.value = "";
+  saveData();
+  render(myLeads);
+});
 function saveData() {
-  let sLeads = JSON.stringify(leads);
-  localStorage.setItem("myLeads", sLeads);
+  let dataSaved = JSON.stringify(myLeads);
+  localStorage.setItem("dataStore", dataSaved);
 }
